0ae1ac4a54b0b5aad385950f7e1bdd591abbdb8c
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-09-01 17:08:25 -0300

    Importing Css and Images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
  import React, { useState } from 'react';
  import Header from './components/Header';
  
+
+ import './App.css';
+
+ import imageBackground from './assets/background.jpg';
 //The component is a function that return HTML ~for now
 //Property: Send info from a child to a parent (like a HTML atribute) 
 //State: Any variable that could be updated by the user should be used in a state
@@ -12,7 +16,7 @@
     const [projects, setProjects] = useState(['Desenv de App', 'Front End Web']);
 
     function handleAddProject(){
-        
+        //... = spreadOperator
         //Changing the variable projects by copying projects and adding a new item by the end of it
         setProjects([ ...projects, `New Project ${Date.now()}`]);
 
@@ -22,6 +26,9 @@
         //Fragments: Create a container to separate components w/o using HTML DOM
         <>
             <Header title="HomePage">
+
+                <img width={300} src={imageBackground}/>
+
                 <ul>
                     {projects.map(project => <li key={project}>{project}</li>)}
                 </ul>
