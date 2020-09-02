0375b44bdec1d7bb5dbe257b8c8bcf328307de23
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-09-01 23:28:10 -0300

    Connecting to an API in NodeJS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
- import React, { useState } from 'react';
+ import React, { useState, useEffect } from 'react';
  import Header from './components/Header';
- 
+ import Api from './services/api.js';
 
  import './App.css';
 
  import imageBackground from './assets/background.jpg';
+import api from './services/api.js';
 //The component is a function that return HTML ~for now
 //Property: Send info from a child to a parent (like a HTML atribute) 
 //State: Any variable that could be updated by the user should be used in a state
@@ -13,7 +14,17 @@
  function App() {
 
     //Deconstructing the variable and the function that will update the variable
-    const [projects, setProjects] = useState(['Desenv de App', 'Front End Web']);
+    const [projects, setProjects] = useState([]);
+
+    //Trigger a function when a parameter is updated
+    //First param is the function to be called. 
+    //The second parameter is the updated variable. 
+    //If is empty, it will trigger the function when the component finished loading 
+    useEffect(()=>{
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        })
+    },[])
 
     function handleAddProject(){
         //... = spreadOperator
@@ -30,7 +41,7 @@
                 <img width={300} src={imageBackground}/>
 
                 <ul>
-                    {projects.map(project => <li key={project}>{project}</li>)}
+                    {projects.map(project => <li key={project.id}>{project.title}</li>)}
                 </ul>
             </Header>
 
